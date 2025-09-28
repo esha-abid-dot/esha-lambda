d7a4026f89f0952cba739f3757b8a137bd4bc51b
esha-abid-dot <your-github-eshaabid62@gmail.com>
2025-09-28 20:06:49 +0500

    Pipeline test update

--- node.js
+++ node.js
@@ -3,6 +3,6 @@ exports.handler = async (event) => {
     console.log("Lambda deployed from pipeline");
     return {
         statusCode: 200,
-        body: JSON.stringify("Hello from Lambda Function!"),
+        body: JSON.stringify("Hello from Lambda v2!"),
     };
 };
