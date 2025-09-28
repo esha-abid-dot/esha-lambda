14a0444ee19e862cf0bd36d9615c3f42a213e39a
esha-abid-dot <eshaabid62@gmail.com>
2025-09-28 20:38:36 +0500

    Update node.js

--- node.js
+++ node.js
@@ -1,6 +1,6 @@
 // index.js
-exports.handler = async (event) => {
-    console.log("Lambda deployed from pipeline");
+export const handler = async (event) => {
+  console.log("Lambda function invoked!", event);
     return {
         statusCode: 200,
         body: JSON.stringify("Hello from Lambda V2!"),
